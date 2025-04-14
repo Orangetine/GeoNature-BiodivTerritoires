efbc783109857e13e538114f4db34d12ca048263
Sali273 <sali274@hotmail.fr>
2025-04-14 15:20:50 +0200

    corrections

--- start_gunicorn.sh
+++ start_gunicorn.sh
@@ -9,10 +9,10 @@ echo "AppDir: $APP_DIR"
 echo "Settings: $APP_DIR/config/settings.ini"
 
 # activate the virtualenv
-source $APP_DIR/venv/bin/activate
+source $APP_DIR/.venv/bin/activate
 
 mkdir -p $APP_DIR/var/log/
 
 cd $APP_DIR
 
-gunicorn wsgi:app --error-log $APP_DIR/var/log/gn_errors.log --pid="${app_name:-gnbt}.pid" -w "${gun_num_workers:-4}" -t ${gun_timeout:-30} -b "${gun_host:-0.0.0.0}:${gun_port:-8080}"  -n "${app_name:-gnbt}"
+gunicorn wsgi:app --error-log $APP_DIR/var/log/gn_errors.log --pid="${app_name:-gnbt}.pid" -w "${gun_num_workers:-4}" -t ${gun_timeout:-30} -b "${gun_host:-0.0.0.0}:${gun_port:-8090}"  -n "${app_name:-gnbt}"
