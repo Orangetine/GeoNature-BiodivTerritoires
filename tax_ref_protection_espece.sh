ce03c38a742ee16176105d2e05083024c6eb6283
Sali273 <salima.rakass@gmail.com>
2025-04-24 11:39:53 +0200

    deleting useless scripts

--- tax_ref_protection_espece.sh
+++
@@ -1,5 +0,0 @@
-. config/settings.ini # sourcing settings.ini parameters
-
-export PGPASSWORD=$user_pg_pass 
-psql -d $db_name -U $user_pg -h $db_host -p $db_port\
-	-f 'data/taxref_protection_especes.sql' 
