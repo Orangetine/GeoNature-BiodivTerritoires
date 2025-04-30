4bcc0e2455300db9bfd8e7f58486e96daccc14d9
Sali273 <salima.rakass@gmail.com>
2025-04-30 14:09:10 +0200

    delete sh file

--- test_db.sh
+++
@@ -1,7 +0,0 @@
-. config/settings.ini # sourcing settings.ini parameters
-
-export PGPASSWORD=$user_pg_pass 
-psql -d $db_name -U $user_pg -h $db_host -p $db_port\
-	-c "SELECT unnest(string_to_array('$areas', ' '))" 
-
-
