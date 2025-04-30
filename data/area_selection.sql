39756a2b430114148b40b23e966b127f222bb3f3
Sali273 <salima.rakass@gmail.com>
2025-04-30 14:04:12 +0200

    deleting old sql files

--- data/area_selection.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO
-    gn_biodivterritory.l_areas_type_selection (id_type)
-SELECT id_type
-FROM
-    ref_geo.bib_areas_types
-WHERE
-        type_code IN (select unnest(string_to_array(:'_areas',' ')))
-ON CONFLICT DO NOTHING;
--- init_db.sh
+++
@@ -1,8 +0,0 @@
-. config/settings.ini
-
-# Utilisation des variables dans des commandes (par exemple, pour se connecter à la base de données)
-export PGPASSWORD=$user_pg_pass
-psql -d $db_name -U $user_pg -h $db_host -p $db_port \
-	-v _areas=$areas \
-	-f data/init_db.sql
-
