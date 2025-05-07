a7ba6ca99f185ea03e0c52f1142a1745f5baa1c5
Orangetine <salima.rakass@gmail.com>
2025-05-07 09:30:31 +0200

    add foreign table ref_nomenclatures

--- data/gn2/atlas_ref_taxonomie.sql
+++ data/gn2/atlas_ref_taxonomie.sql
@@ -14,3 +14,8 @@ FROM SERVER geonaturedbserver INTO taxonomie ;
 
 ALTER TABLE taxonomie.bdc_statut OWNER TO myuser;
 GRANT ALL ON TABLE taxonomie.bdc_statut TO myuser;
+
+CREATE SCHEMA IF NOT EXISTS ref_nomenclatures;
+
+IMPORT FOREIGN SCHEMA ref_nomenclatures
+FROM SERVER geonaturedbserver INTO ref_nomenclatures ;
