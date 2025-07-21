6efcf2f543107f56b8c2e5e2ee26fed5cc824c2a
Taqarort Mustapha <ta_mus@hotmail.fr>
2025-07-21 13:42:00 +0200

    grant to geonatatlas

--- data/gn2/atlas_ref_taxonomie.sql
+++ data/gn2/atlas_ref_taxonomie.sql
@@ -28,8 +28,22 @@ GRANT ALL ON TABLE ref_nomenclatures.t_nomenclatures TO myuser;
 ALTER TABLE ref_nomenclatures.bib_nomenclatures_types OWNER TO myuser;
 GRANT ALL ON TABLE ref_nomenclatures.bib_nomenclatures_types TO myuser;
 
+DROP FOREIGN TABLE IF EXISTS synthese.cor_sensitivity_area_type CASCADE;
+
+IMPORT FOREIGN SCHEMA gn_sensitivity
+LIMIT TO (cor_sensitivity_area_type) FROM SERVER geonaturedbserver INTO synthese;
+
 GRANT SELECT ON ALL TABLES IN SCHEMA ref_nomenclatures TO geonatatlas;
 GRANT USAGE ON SCHEMA ref_nomenclatures TO geonatatlas;
 
--- GRANT SELECT ON ALL TABLES IN SCHEMA ref_geo TO myuser;
--- GRANT USAGE ON SCHEMA ref_geo TO myuser;
+GRANT USAGE ON SCHEMA atlas to geonatatlas;
+GRANT USAGE ON SCHEMA taxonomie to geonatatlas;
+GRANT USAGE ON SCHEMA gn_biodivterritory to geonatatlas;
+
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA atlas to geonatatlas;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA taxonomie to geonatatlas;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA gn_biodivterritory to geonatatlas;
+
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA atlas to geonatatlas;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA taxonomie to geonatatlas;
+GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA gn_biodivterritory to geonatatlas;
