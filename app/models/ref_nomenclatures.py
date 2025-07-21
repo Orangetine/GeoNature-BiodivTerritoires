f880f9985cfcb00b8aeac6412fd758b50178901b
Taqarort Mustapha <ta_mus@hotmail.fr>
2025-07-21 13:53:19 +0200

    Orm mapping vms instead of foreign table fdw

---
+++ app/models/ref_nomenclatures.py
@@ -0,0 +1,35 @@
+from sqlalchemy import (
+    Column,
+    ForeignKey,
+    Integer,
+    String,
+    Text,
+)
+from sqlalchemy.dialects.postgresql import JSONB
+from utils_flask_sqla.serializers import serializable
+
+from app.core.env import DB
+
+class TNomenclatures(DB.Model):
+    __tablename__ = "vm_t_nomenclatures"
+    __table_args__ = {"schema": "atlas"}
+
+    id_nomenclature = Column(Integer, primary_key=True)
+    cd_nomenclature = Column(String(255))
+    mnemonique = Column(String(255))
+    definition_fr = Column(Text)
+
+    id_type = Column(
+        Integer, ForeignKey("atlas.vm_bib_nomenclatures_types.id_type")
+    )
+
+
+@serializable
+class BibNomenclaturesTypes(DB.Model):
+    __tablename__ = "vm_bib_nomenclatures_types"
+    __table_args__ = {"schema": "atlas"}
+    id_type = Column(Integer, primary_key=True)
+    mnemonique = Column(String(255))
+
+
+
